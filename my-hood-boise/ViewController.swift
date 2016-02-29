ac7cbfdf45b7fd5af2a193e38ad5c1a628cfe67d
TGrooves-208 <tgrooves.208@gmail.com>
2016-02-29 00:16:34 -0700

    Created Table View, Loaded Test Data, Created A Custom UI Table View Cell, Implemented Table View Delegate & Table View Data Source, Set the items in a row and height in each cell, Created a Custom Post Cell. Connected a IBOutlet then set the relationship to the Mainstoryboard.

--- my-hood-boise/ViewController.swift
+++ my-hood-boise/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        var post1 = Post(imagePath: "", title: "Post 1 test", description: "Post 1 description test")
+        var post2 = Post(imagePath: "", title: "Post 2 test", description: "Post 2 description test")
+        var post3 = Post(imagePath: "", title: "Post 3 test", description: "Post 3 description test")
+        
+        posts.append(post1)
+        posts.append(post2)
+        posts.append(post3)
+        
+        tableView.reloadData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell {
+            cell.configureCell(post)
+            return cell
+            
+        } else {
+            let cell = PostCell()
+            cell.configureCell(post)
+            return cell
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        return 80.0
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
 
 }
 
